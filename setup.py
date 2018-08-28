65e8ac1e4365129a1d440b25664f1db332f5e0d0
Liviu Manea <mlmarius@yahoo.com>
2018-08-28 15:48:33 +0300

    restructuring plugin

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='eposfederator.plugins.radon',  # Required
+    name='eposfederator.federators.radon',  # Required
     version='0.0.1',  # Required
     description='Epos Federation for the Radon APIs',  # Required
     package_dir={'': 'src'},
     packages=find_packages('src'),
-    namespace_packages=['eposfederator', 'eposfederator.plugins'],
+    entry_points={'eposfederator.federators': 'radon = eposfederator.federators.radon'},
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     zip_safe=False,
-    package_data={'eposfederator.plugins.radon': ['settings.yml']}
+    package_data={'eposfederator.federators.radon': ['settings.yml']}
 )
